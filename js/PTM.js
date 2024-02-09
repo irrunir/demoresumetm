98e3f3d7c98384f40f794f06556a959c43bd8827
Ali <155351577+userali-ir@users.noreply.github.com>
2024-02-10 01:44:48 +0330

    Resume Demo TM

---
+++ js/PTM.js
@@ -0,0 +1,79 @@
+/* WebSite > www.PersianCommunity.ir */
+/* By > www.userali.ir */
+
+console.clear();
+
+console.log(
+"                                                                                   \n" +
+"%c                                 @@       %c      @@                               \n" +
+"%c                            @@@@@@@@@@    %c   @@@@@@@@@@                          \n" +
+"%c                        @@@@@@@@@@@@@@    %c   @@@@@@@@@@@@@@/                     \n" +
+"%c                   /@@@@@@@@@@@@@@@@@     %c    @@@@@@@@@@@@@@@@@@                 \n" +
+"%c               @@@@@@@@@@@@@@@@@@         %c        @@@@@@@@@@@@@@@@@@             \n" +
+"%c           @@@@@@@@@@@@@@@@@@             %c            %@@@@@@@@@@@@@@@@@.        \n" +
+"%c         @@@@@@@@@@@@@@@%                 %c                 @@@@@@@@@@@@@@@       \n" +
+"%c         @@@@@@@@@@@@@@@(                 %c                 @@@@@@@@@@@@@@@       \n" +
+"%c           @@@@@@@@@@@@@@@@@@             %c            #@@@@@@@@@@@@@@@@@*        \n" +
+"%c               @@@@@@@@@@@@@@@@@@         %c        @@@@@@@@@@@@@@@@@@             \n" +
+"%c    &@@(           *@@@@@@@@@@@@@@@@@/    %c    @@@@@@@@@@@@@@@@@@           &@@&  \n" +
+"%c  @@@@@@@@@@            @@@@@@@@@@@@@@@@@@%c@@@@@@@@@@@@@@@@@/           @@@@@@@@@@\n" +
+"%c @@@@@@@@@@@@@@@@           @@@@@@@@@@@@@@%c@@@@@@@@@@@@@           ,@@@@@@@@@@@@@@\n" +
+"%c @@@@@@@@@@@@@@@@@@@@           &@@@@@@@@@%c@@@@@@@@@           @@@@@@@@@@@@@@@@@@@\n" +
+"%c @@@@@@@@@@@@@@@@@@@@@@@@            @@@@@%c@@@@%           @@@@@@@@@@@@@@@@@@@@@@@\n" +
+"%c @@@@@@@@@ ,@@@@@@@@@@@@@@@@@@            %c           ,@@@@@@@@@@@@@@@@@& @@@@@@@@\n" +
+"%c @@@@@@@@@      @@@@@@@@@@@@@@@@@@        %c       @@@@@@@@@@@@@@@@@@      @@@@@@@@\n" +
+"%c @@@@@@@@@          @@@@@@@@@@@@@@@@@@    %c   @@@@@@@@@@@@@@@@@@          @@@@@@@@\n" +
+"%c @@@@@@@@@              .@@@@@@@@@@@@@@   %c  @@@@@@@@@@@@@@%              @@@@@@@@\n" +
+"%c @@@@@@@@@                   @@@@@@@@@@   %c  @@@@@@@@@@,                  @@@@@@@@\n" +
+"%c @@@@@@@@@                    %@@@@@@@@   %c  @@@@@@@@@                    @@@@@@@@\n" +
+"%c @@@@@@@@@                    %@@@@@@@@   %c  @@@@@@@@@                    @@@@@@@@\n" +
+"%c  @@@@@@@                     %@@@@@@@@   %c  @@@@@@@@@                     @@@@@@(\n" +
+"%c                              %@@@@@@@@   %c  @@@@@@@@@                            \n" +
+"%c                              %@@@@@@@@   %c  @@@@@@@@@                            \n" +
+"%c            @@@@@             %@@@@@@@@   %c  @@@@@@@@@              @@@@          \n" +
+"%c          @@@@@@@@@@@         %@@@@@@@@   %c  @@@@@@@@@         @@@@@@@@@@@        \n" +
+"%c          @@@@@@@@@@@@@@@*    %@@@@@@@@   %c  @@@@@@@@@     @@@@@@@@@@@@@@@        \n" +
+"%c            @@@@@@@@@@@@@@@@@@%@@@@@@@@   %c  @@@@@@@@@ @@@@@@@@@@@@@@@@@&         \n" +
+"%c                @@@@@@@@@@@@@@@@@@@@@@@   %c  @@@@@@@@@@@@@@@@@@@@@@@,             \n" +
+"%c                    #@@@@@@@@@@@@@@@@@@   %c  @@@@@@@@@@@@@@@@@@@                  \n" +
+"%c                        .@@@@@@@@@@@@@@   %c  @@@@@@@@@@@@@@@                      \n" +
+"%c                             @@@@@@@@@@   %c  @@@@@@@@@@,                          \n" +
+"%c                                                                                   \n" +
+"%c                               https://PTM.ir (:                                \n",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #3949AB;font-weight: bold;",
+"color: #D32F2F;font-weight: bold;", "color: #D32F2F;font-weight: bold;");
+
+/* WebSite > www.PersianCommunity.ir */
+/* By > www.userali.ir */
